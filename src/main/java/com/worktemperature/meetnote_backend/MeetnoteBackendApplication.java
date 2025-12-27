3e25d4fb7b5fdf25572d89bf81f8d5fef5d7611b
passiongyu <passiongyu7606@gmail.com>
2025-12-27 19:53:12 +0900

    백엔드 init

---
+++ src/main/java/com/worktemperature/meetnote_backend/MeetnoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.worktemperature.meetnote_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetnoteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetnoteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worktemperature/meetnote_backend/MeetnoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worktemperature.meetnote_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetnoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
